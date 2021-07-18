8ce638525b38bc35f3a86696dbd960aa680682f7
SaltySugar32 <45869757+SaltySugar32@users.noreply.github.com>
2021-07-18 21:40:46 +1000

    edited api/products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,9 +10,11 @@ class ProductsController extends Controller
 {
     public function index(Request $request){
         if ($request->category_id!=null){
+            if(Product::where('category_id', $request->category_id)->first()!=null)
             return response()->json(ProductArticle::whereIn('product_id',
                 Product::where('category_id', $request->category_id)->get('id')
             )->get());
+            return response(null,404);
         }
         $productarticles=ProductArticle::all();
         $data=[];
@@ -30,17 +32,21 @@ class ProductsController extends Controller
         return response()->json($data);
     }
 
-    public function show($index){
+    public function show($index)
+    {
         $productarticle = ProductArticle::where('id', $index)->first();
-        $product = Product::where('id', $productarticle->product_id)->first();
-        $data=[];
-        array_push($data, [
-            'title'=>$productarticle->title,
-            'description'=>$productarticle->description,
-            'short_description'=>$productarticle->short_description,
-            'picture_link'=>$productarticle->picture_link,
-            'price'=>$product->price,
-            'category_id'=>$product->category_id]);
-        return response()->json($data);
+        if ($productarticle != null) {
+            $product = Product::where('id', $productarticle->product_id)->first();
+            $data = [];
+            array_push($data, [
+                'title' => $productarticle->title,
+                'description' => $productarticle->description,
+                'short_description' => $productarticle->short_description,
+                'picture_link' => $productarticle->picture_link,
+                'price' => $product->price,
+                'category_id' => $product->category_id]);
+            return response()->json($data);
+        }
+        return response(null,404);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <p>Здрасьте, с подключениемм</p>
+        <p>Здрасьте, с подключением</p>
     </body>
 </html>
 
